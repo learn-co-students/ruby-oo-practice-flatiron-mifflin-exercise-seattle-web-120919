bf86105a75638ef1030c542f6682d1114ded81ed
christinamcmahon <christina.mcmahon1@gmail.com>
2019-12-16 13:26:24 -0800

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,33 @@
 class Manager
+    attr_reader :name, :department
+    attr_accessor :age
+    @@all = []
 
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.all_departments
+        Manager.all.map{|manager| manager.department}.uniq
+    end
+
+    def self.average_age
+        ages = Manager.all.map{|manager| manager.age}
+        ages.sum / ages.size.to_f
+    end
+
+    def employees
+        Employee.all.find_all{|employee| employee.manager == self}
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self)
+    end
 end
--- run.rb
+++ run.rb
@@ -4,6 +4,11 @@ require 'pry'
 
 
 #Test your code here
+michael = Manager.new("Michael", "Paper", 35)
+christina = Manager.new("Christina", "IT", 27)
+jim = Employee.new("Jim", 40000, michael)
+sean = Employee.new("Sean", 50000, christina)
+michael.hire_employee("Dwight", 35000)
 
 
 binding.pry
