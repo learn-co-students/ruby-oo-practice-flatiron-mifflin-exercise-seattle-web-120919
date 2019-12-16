6d685268e5172c16dfbe5ff246929d6bd89f713b
github username <github email address>
2019-12-16 14:40:33 -0800

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,33 @@
+require_relative "Manager"
+require 'pry'
 class Employee
+    attr_accessor :name, :manager_name, :salary
+    @@all = []
+    def initialize(name, salary, manager_name)
+        @name = name
+        @manager_name = manager_name
+        @salary = salary
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end 
+
+    def self.paid_over(over)
+        self.all.select{|employee| employee.salary > over}
+    end 
+
+    def self.find_by_department(department)
+        dep_manager = Manager.all.select{|manager| 
+            manager.department==department}
+        dep_manager[0].employees[0]
+    end
+
+    def tax_bracket
+        Employee.all.select{|employee| 
+            -1000 <= (employee.salary - self.salary) &&  
+            (employee.salary - self.salary) <= 1000 && 
+            employee.name != self.name}
+    end     
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,57 @@
+require 'pry'
+require_relative "Employee"
 class Manager
+    attr_accessor :name, :age
+    attr_reader :department
+    @@all = []
 
+    def initialize(name, department, age)
+        if(departmentCheck(department))
+            @name = name
+            @age = age
+            @department = department
+            @@all << self
+        end 
+    end
+
+    def self.all
+        @@all
+    end 
+    def department=(department)
+         if(!departmentCheck(department))
+            @department = department
+         end
+    end 
+
+    def departmentCheck(department)
+        if(Manager.all.find {|manager| 
+        manager.department == department})
+            return false
+        else 
+            return true
+        end 
+    end  
+
+    def self.all_departments
+        self.all.map {|manager| manager.department}
+    end 
+
+    def self.average_age 
+        ages = 0.0
+        count = 0.0
+        self.all.each do |manager| 
+            ages += manager.age
+            count += 1
+        end 
+        return (ages/count).round(2)
+    end
+     
+    def hire_employee(name, salary)
+        employee = Employee.new(name, salary, self.name)
+    end
+
+    def employees
+        Employee.all.select{ |employee| 
+            employee.manager_name == self.name }
+    end
 end
