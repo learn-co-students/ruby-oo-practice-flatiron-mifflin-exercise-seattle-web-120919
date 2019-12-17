9826ea921aade1bee7b0f49aae08f75faebaead0
github username <github email address>
2019-12-16 18:21:33 -0800

    Done.

--- run.rb
+++ run.rb
@@ -2,8 +2,14 @@ require_relative "lib/Manager"
 require_relative "lib/Employee"
 require 'pry'
 
+m1 = Manager.new("Chris", "Coding", 31)
+m2 = Manager.new("Sandarya", "Not Coding", 25)
 
-#Test your code here
+e1 = Employee.new("Eric", 75000, m1)
+e2 = Employee.new("Cole", 80000, m2)
+e3 = Employee.new("Christina", 85000, m1)
+e4 = Employee.new("Eli", 90000, m1)
+
 
 
 binding.pry
